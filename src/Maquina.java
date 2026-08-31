4c9cc59813e69c0df45a581007db815389f43871
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-08-31 13:24:45 -0300

    Atributos: atributos e construtor da classe Maquina

--- src/Maquina.java
+++ src/Maquina.java
@@ -1,2 +1,12 @@
 public class Maquina {
+    private String nome;
+    private boolean ligada;
+    private int capacidadeMaxima;
+
+    public Maquina(String nome, int capacidadeMaxima) {
+        this.nome = nome;
+        ligada = false;
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
 }
