d7aa6f2c8023093ca497ce69120a29384cd1dbcb
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-08-31 00:16:51 -0300

    Métodos: consumir e adicionarEstoque

--- src/MateriaPrima.java
+++ src/MateriaPrima.java
@@ -1,16 +1,32 @@
 public class MateriaPrima {
-  private String id;
-  private String nome;
-  private int quantidade;
-  private String unidade;
-  private int quantidadeMinima;
+    private String id;
+    private String nome;
+    private int quantidade;
+    private String unidade;
+    private int quantidadeMinima;
 
-  public MateriaPrima(
-      String id, String nome, int quantidade, String unidade, int quantidadeMinima) {
-    this.id = id;
-    this.nome = nome;
-    this.quantidade = quantidade;
-    this.unidade = unidade;
-    this.quantidadeMinima = quantidadeMinima;
-  }
+    public MateriaPrima(
+            String id, String nome, int quantidade, String unidade, int quantidadeMinima) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public boolean consumir(int demanda) {
+        if (demanda <= 0 || demanda > quantidade) {
+            return false;
+        }
+        quantidade -= demanda;
+        return true;
+    }
+
+    public boolean adicionarEstoque(int quantidadeAdicionada) {
+        if (quantidadeAdicionada <= 0) {
+            return false;
+        }
+        quantidade += quantidadeAdicionada;
+        return true;
+    }
 }
