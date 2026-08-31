8e2802b447489f8de89c10981c95749a8206ade5
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-08-31 08:13:22 -0300

    Atributos: atribuicao de atributos e construtor de Produtos

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,18 @@
 public class Produto {
+    private String id;
+    private String nome;
+    private StatusProduto status;
+    private int quantidadeMateriaPrimaNecessaria;
+    // Atributo materiaPrima criado para permitir rastrear a materia-prima
+    // utilizada em cada tipo de Produto
+    private MateriaPrima materiaPrima;
+
+    public Produto(
+            String id, String nome, int quantidadeMateriaPrimaNecessaria) {
+        this.id = id;
+        this.nome = nome;
+        this.status = StatusProduto.AGUARDANDO_PROCESSAMENTO;
+        this.quantidadeMateriaPrimaNecessaria = quantidadeMateriaPrimaNecessaria;
+    }
+
 }
