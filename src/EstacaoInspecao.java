a0b3759d29fc12e6b4b82e69be334313a768f99e
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-09-02 11:06:38 -0300

    Classe EstacaoInspecao - Victor

--- src/EstacaoInspecao.java
+++ src/EstacaoInspecao.java
@@ -1,2 +1,42 @@
 public class EstacaoInspecao {
+    // Atributos
+    private boolean ativa;
+    private int produtosInspecionados;
+
+    // Construtor
+    public EstacaoInspecao() {
+        ativa = false;
+        produtosInspecionados = 0;
+    }
+
+    // Ativa a estação
+    public void ativar() {
+        ativa = true;
+    }
+
+    // Desativa a estação
+    public void desativar() {
+        ativa = false;
+    }
+
+    public boolean inspecionar(String produto) {
+        if (!ativa) {
+            return false;
+        }
+
+        if (produto == null) {
+            return false;
+        }
+
+        produtosInspecionados++;
+        return true;
+    }
+
+    public int getTotalInspecionados() {
+        return produtosInspecionados;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
 }
