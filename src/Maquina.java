68dc8ab09936be340814370b891bb3c9a112a442
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-09-02 17:06:36 -0300

    Método: transformacao da matéria-prima em produto

--- src/Maquina.java
+++ src/Maquina.java
@@ -12,8 +12,28 @@ public class Maquina {
     public void ligar() {
         ligada = true;
     }
+
     public void desligar() {
         ligada = false;
     }
-    public processar(MateriaPrima materiaPrima, Produto )
+
+    public boolean processar(MateriaPrima materiaPrima, Produto produto, int demanda) {
+        if (!ligada) {
+            return false;
+        }
+        if (demanda > capacidadeMaxima) {
+            return false;
+        }
+
+        // Matéria-prima é gasta do estoque
+        if (!materiaPrima.verificarDisponibilidade(demanda)) {
+            return false;
+        }
+        // O produto vira PROCESSADO e registra a origem
+        if (!materiaPrima.consumir(demanda)) {
+            return false;
+        }
+
+        return produto.processar(materiaPrima);
+    }
 }
