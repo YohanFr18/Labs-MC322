b15cef6143454d579ab314a1df94a799cc45e8ed
Yohan Rocha <yohanfs.rocha@gmail.com>
2026-09-15 17:23:53 -0300

    Classe: implementacao da subclasse TuringX4, herda Produto

---
+++ src/TuringX4.java
@@ -0,0 +1,26 @@
+public class TuringX4 extends Produto {
+    public TuringX4() {
+        super("Turing-X4", 120, 0.5f);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Processador de Baixa Qualidade";
+    }
+
+    @Override
+    public int calcularTempoProducao() {
+        int tempoBase = 30;
+        return tempoBase + (getQuantidadeMateriaPrimaPorUnidade() / 10) + (int) (getQualidade() * 30);
+    }
+
+    @Override
+    public boolean processar() {
+        if (getStatus() != StatusProduto.AGUARDANDO_PROCESSAMENTO) {
+            return false;
+        }
+
+        setStatus(StatusProduto.PROCESSADO);
+        return true;
+    }
+}
